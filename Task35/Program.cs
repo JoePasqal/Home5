0fa43c8ee1cd08c733b01c263977eb7895f7392f
Vladimir GYM Shcheglov <joepasqal@yandex.ru>
2023-06-30 00:39:46 +0500

    soda2

---
+++ Task35/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int [] GetRandomArray()
+{
+    int[] array = new int[10];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = Random.Shared.Next(100, 1000);
+    }
+    return array;
+}
+int[] array = GetRandomArray();
+int chetnie = 0;
+for(int i = 0; i < array.Length; i++)
+{
+if(array[i] % 2 == 0)
+    {
+chetnie++;
+    }
+}
+
+Console.WriteLine("Количество четных чисел:");
+Console.WriteLine(String.Join(" , ", chetnie));
---
+++ Task36/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int [] GetRandomArray()
+{
+    int[] array = new int[5];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = Random.Shared.Next(-9, 10);
+    }
+    return array;
+}
+int[] array = GetRandomArray();
+int sum = 0;
+for(int i = 1; i < array.Length; i+=2)
+{
+    sum += array[i];
+}
+Console.WriteLine($"Сумма нечетных элементов равна = {sum}");
